73da8405c8afd884ca8d40578fad43eb74fb47dd
mafuth <mafaaz708@gmail.com>
2023-03-12 18:43:08 +0500

    updates

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,8 @@
+$('a').click(function(){
+    if(this.attr('target') != "_blank" && this.attr('href') !="" && this.attr('href') !="#"){
+        $('#pre_loader').show();
+    }
+});
 window.addEventListener("load", function(){
     $('#pre_loader').hide();
 });
